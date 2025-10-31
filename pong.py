5c3d3c662d8cad96535cc0c8d41339c90319d872
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-31 18:57:44 +0100

    Add court

--- pong.py
+++ pong.py
@@ -7,13 +7,18 @@ clock = pygame.time.Clock()
 running = True
 
 GRAVITY = 0.5
-GROUND_Y = 950
+GROUND_Y = pygame.Surface.get_height(screen)
 
 class Court:
     def __init__(self, x, y):
-        self.x = x
-        self.x = y
-        self.rectangle = 0
+        self.left = 10
+        self.top = 10
+        self.x = x - 20
+        self.y = y - 20
+        self.rectangle = (self.left, self.top, self.x, self.y)
+        self.color = (255, 255, 255)
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rectangle)
 
 class Ball:
     def __init__(self, x, y):
@@ -36,6 +41,7 @@ class Ball:
 
 
 ball = Ball(300, 100)
+court = Court(pygame.Surface.get_width(screen), pygame.Surface.get_height(screen))
 
 while running:
     for event in pygame.event.get():
@@ -46,6 +52,7 @@ while running:
 
     screen.fill("black")
     ball.draw(screen)
+    court.draw(screen)
     pygame.display.flip()
 
     clock.tick(60)
