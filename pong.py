41490902d3f33d3647229484bfb41b8327b9ed29
Sayf <soufiane.bakouri.dev@gmail.com>
2025-10-31 19:33:21 +0100

    add player class

--- pong.py
+++ pong.py
@@ -6,8 +6,19 @@ screen = pygame.display.set_mode((600, 1000))
 clock = pygame.time.Clock()
 running = True
 
-GRAVITY = 0.5
+gravity = 1
 GROUND_Y = screen.get_height()
+
+class Player:
+    def __init__(self, left, top, x, y, padding, color):
+        self.left = left
+        self.top = top
+        self.x = x - padding
+        self.y = y - padding
+        self.rectangle = (self.left, self.top, self.x, self.y)
+        self.color = color
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rectangle, 0, 10)
 
 class Court:
     def __init__(self, left, top, x, y, padding, color):
@@ -24,25 +35,26 @@ class Ball:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = 50
+        self.radius = 20
         self.color = (255, 255, 255)
         self.vy = 0
 
     def update(self):
-        self.vy += GRAVITY
+        self.vy += gravity
         self.y += self.vy
 
         if self.y + self.radius > GROUND_Y:
             self.y = GROUND_Y - self.radius
-            self.vy *= -0.7
+            self.vy *= -1
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.x), int(self.y)), self.radius)
 
 
-ball = Ball(300, 100)
+ball = Ball(screen.get_width()/2, 0)
 court = Court(10, 10, screen.get_width(), screen.get_height(), 20,(255, 255, 255))
 blackcourt = Court(20 , 20, screen.get_width(), screen.get_height(), 40,(0, 0, 0))
+playerone = Player(20 , 20, screen.get_width()/2, 100, 40,(255, 255, 255))
 
 
 while running:
@@ -56,6 +68,7 @@ while running:
     court.draw(screen)
     blackcourt.draw(screen)
     ball.draw(screen)
+    playerone.draw(screen)
     pygame.display.flip()
 
     clock.tick(60)
